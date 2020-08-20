938a2ef13059519e8d589ff3f06db90a1955d4cc
Kierre Willoughby <kwilloughy1@uco.edu>
2020-08-20 14:29:46 -0500

    changed title added new code

---
+++ HelloWorld/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(final String[] args) {
+
+		final JFrame window = new JFrame();
+		window.setSize(500,300);
+		window.setLocation(400, 200);
+
+		window.pack();
+		window.setVisible(true);
+
+	}
+}
