33873b2b8fcdd619b713538bd5cdc7c9420d0288
Kierre Willoughby <kwilloughy1@uco.edu>
2020-09-03 18:50:54 -0500

    done wld like to + barriers & xtra features

---
+++ creative1/model/Order.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Order {
+
+	String[][] size = new String[][]{{"Personal, ", "5.99"}, {"Small, ", "7.99"}, {"Medium, ", "11.99"},
+	{"Large, ", "13.99"}, {"Colossal, ", "25.99"}};//done
+	String[][] crust = new String[][]{{"Thin, ", "0.00"}, {"Pan, ", "0.00"}, {"Deep, ", "1.00"}};//done
+	String[] sauce= new String[]{"Alfredo, ", "Traditional, ", "BBQ "};//done
+	String[] toppings= new String[]{"Peperoni, ", "Sausage, ", "Hamburger, ", "Ham, " , "Chicken, ",
+	"Onions, ", "Spinach, ", "Pineapple, ", "Peppers, ", "Olives, "};//done
+	String[][] sides= new String[][]{{"Breadsticks, ", "2.00"}, {"Wings, ", "5.99"}, 
+	{"Cheesy Bread, ", "2.00"}, {"Side Salad, ", "1.00"}};
+	String[][] dessert= new String[][]{{"Cookies, ", "2.00"}, {"Desserts Stix, ", "2.00"}};
+	String[][] extras= new String[][]{{"Garlic Sauce, ", "0.50"}, {"Ranch, ", "0.50"}, 
+	{"Bleu CHeese, ", "0.50"}, {"Pamesean and Peppers, ", "0.00"}};//done
+	int numDrinks;
+	public double orderTotal;
+	public String description;
+
+	public Order(){
+		/*panel.init();
+		numDrinks = 0;
+		orderTotal = 0;
+		description = "";*/
+	}
+
+	public String[][] getSize(){
+		return size;
+	}
+
+	public String[][] getCrust(){
+		return crust;
+	}
+
+	public String[] getSauce(){
+		return sauce;
+	}
+
+	public String[] getToppings(){
+		return toppings;
+	}
+
+	public String[][] getExtras (){
+		return extras;
+	}
+
+	public String[][] getSides (){
+		return sides;
+	}
+
+	public String[][] getDessert (){
+		return dessert;
+	}
+}
--- creative1/view/MenuScreen.java
+++ creative1/view/MenuScreen.java
@@ -2,17 +2,20 @@ package view;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.GridLayout;
-
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
+import control.ButtonClickListener;
+import model.Order;
 
 public class MenuScreen {
 
@@ -20,11 +23,14 @@ public class MenuScreen {
 	private JRadioButton[] sizeButtons;
 	private JRadioButton[] crustButtons;
 	private JRadioButton[] sauceButtons;
-	private JRadioButton[] fanButtons;
 	private JCheckBox[] sidesCheckBox;
 	private JCheckBox[] desCheckBox;
 	private JCheckBox[] extrasCheckBox;
 	private JCheckBox[] topCheckBox;
+	private JLabel lblDrink = new JLabel("Number of drinks. You want it? We got it don't worry! $1.00/drink");
+	private	JTextField txtDrink = new JTextField("Enter drink amount. Must enter amount");
+	private	JButton orderButton = new JButton("Place order");
+	public Order o1 = new Order();
 
 	public MenuScreen(JFrame window){
 		this.window = window;
@@ -35,7 +41,9 @@ public class MenuScreen {
 		Container cp = window.getContentPane();
 		JPanel pizzaPanel = new JPanel();
 		JPanel xtraPanel = new JPanel();
-		//panel.setPreferredSize(new Dimension(800, 400));
+		JPanel drinkPanel = new JPanel();
+		drinkPanel.setLayout(new GridLayout(2, 2));
+		
 
 		//sets up the size panel
 		sizeButtons = new JRadioButton[5];
@@ -60,7 +68,7 @@ public class MenuScreen {
 		crustButtons = new JRadioButton[3];
 		crustButtons[0] = new JRadioButton("Thin");
 		crustButtons[1] = new JRadioButton("Pan");
-		sizeButtons[1].setSelected(true);
+		crustButtons[1].setSelected(true);
 		crustButtons[2] = new JRadioButton("Deep dish + $1.00 \n avalible for small - large pizzas");
 
 		JPanel crustPanel = new JPanel();
@@ -91,43 +99,25 @@ public class MenuScreen {
 		pizzaPanel.add(saucePanel);
 
 		//sets ups toppings pannel
-		sidesCheckBox = new JCheckBox[10];
-		sidesCheckBox[0] = new JCheckBox("Peperoni");
-		sidesCheckBox[1] = new JCheckBox("Sausage");
-		sidesCheckBox[2] = new JCheckBox("Hamburger");
-		sidesCheckBox[3] = new JCheckBox("Ham");
-		sidesCheckBox[4] = new JCheckBox("Chicken");
-		sidesCheckBox[5] = new JCheckBox("Onions");
-		sidesCheckBox[6] = new JCheckBox("Spinach");
-		sidesCheckBox[7] = new JCheckBox("pineapple");
-		sidesCheckBox[8] = new JCheckBox("Peppers");
-		sidesCheckBox[9] = new JCheckBox("Olives");
+		topCheckBox = new JCheckBox[10];
+		topCheckBox[0] = new JCheckBox("Peperoni");
+		topCheckBox[1] = new JCheckBox("Sausage");
+		topCheckBox[2] = new JCheckBox("Hamburger");
+		topCheckBox[3] = new JCheckBox("Ham");
+		topCheckBox[4] = new JCheckBox("Chicken");
+		topCheckBox[5] = new JCheckBox("Onions");
+		topCheckBox[6] = new JCheckBox("Spinach");
+		topCheckBox[7] = new JCheckBox("pineapple");
+		topCheckBox[8] = new JCheckBox("Peppers");
+		topCheckBox[9] = new JCheckBox("Olives");
 
 		JPanel topPanel = new JPanel();
 		topPanel.setLayout(new GridLayout(4,1));
 		topPanel.setBorder(BorderFactory.createTitledBorder("Toppings $0.50 each"));
-		for(var b: sidesCheckBox){
+		for(var b: topCheckBox){
 			topPanel.add(b);
 		}
 		pizzaPanel.add(topPanel);
-
-		//sets ups fan favorites panel
-		fanButtons = new JRadioButton[5];
-		fanButtons[0] = new JRadioButton("Meat Lovers");
-		fanButtons[1] = new JRadioButton("Veggie");
-		fanButtons[2] = new JRadioButton("Hawaiian");
-		fanButtons[3] = new JRadioButton("CHicken Alfredo");
-		fanButtons[4] = new JRadioButton("BBQ CHicken");
-
-		JPanel fanPanel = new JPanel();
-		fanPanel.setLayout(new GridLayout(5,1));
-		fanPanel.setBorder(BorderFactory.createTitledBorder("Fan favorites"));
-		ButtonGroup fanGroup = new ButtonGroup();
-		for(var b: fanButtons){
-			fanPanel.add(b);
-			fanGroup.add(b);
-		}
-		xtraPanel.add(fanPanel);
 
 		// sets up sides panel
 		sidesCheckBox = new JCheckBox[4];
@@ -172,9 +162,148 @@ public class MenuScreen {
 		}
 		xtraPanel.add(extrasPanel);
 
+		
+		drinkPanel.add(lblDrink);
+		drinkPanel.add(txtDrink);
+		drinkPanel.add(orderButton);
+
 		cp.add(BorderLayout.CENTER, xtraPanel);
 		cp.add(BorderLayout.NORTH, pizzaPanel);
-
-
+		cp.add(BorderLayout.SOUTH, drinkPanel);
+		orderButton.addActionListener(new ButtonClickListener(this));
 	}
+
+	public JButton getOrderButton(){
+		return orderButton;
+	}
+
+	public JCheckBox[] getToppingsButtons(){
+		return topCheckBox;
+	}
+
+	public JCheckBox[] getExtrasButtons(){
+		return extrasCheckBox;
+	}
+
+	public JCheckBox[] getSidesButtons(){
+		return sidesCheckBox;
+	}
+
+	public JCheckBox[] getDessertButtons(){
+		return desCheckBox;
+	}
+
+	public JTextField getDrinks(){
+		return txtDrink;
+	}
+
+	public JFrame getWindow(){
+		return window;
+	}
+
+	public void determSize(){
+		String[][] size = o1.getSize();
+		int i = 0;
+		for(var b : sizeButtons){
+			if(b.isSelected()){
+				o1.orderTotal += Double.parseDouble(size[i][1]);
+				o1.description = "Pizza description: " + size[i][0];
+				break;
+			}
+			i++;
+		}
+	}
+
+	public void determCrust (){
+		String[][] crust = o1.getCrust();
+		int i = 0;
+		for(var b : crustButtons){
+			if(b.isSelected()){
+				o1.orderTotal += Double.parseDouble(crust[i][1]);
+				o1.description += crust[i][0];
+				break;
+			}
+			i++;
+		}
+	}
+
+	public void determSauce(){
+		String[] sauce = o1.getSauce();
+		int i = 0;
+		for(var b : sauceButtons){
+			if(b.isSelected()){
+				o1.description += sauce[i];
+				break;
+			}
+			i++;
+		}
+		o1.description += "\n" + "Toppings: ";
+	}
+
+	public void determToppings(){
+		String[] toppings = o1.getToppings();
+		int i = 0;
+		for(var b : topCheckBox){
+			if(b.isSelected()){
+				o1.description += toppings[i];
+				o1.orderTotal += 0.50; 
+			}
+			i++;
+		}
+	}
+
+	public void determExtras (){
+		o1.description += "\n" + "Extras: ";
+		String[][] extras = o1.getExtras();
+		int i = 0;
+		for(var b : extrasCheckBox){
+			if(b.isSelected()){
+				o1.description += extras[i][0];
+				o1.orderTotal += Double.parseDouble(extras[i][1]); 
+			}
+			i++;
+		}
+	}
+
+		public void determSides (){
+			o1.description += "\n" + "Sides: ";
+			String[][] sides = o1.getSides();
+			int i = 0;
+			for(var b : sidesCheckBox){
+				if(b.isSelected()){
+					o1.description += sides[i][0];
+					o1.orderTotal += Double.parseDouble(sides[i][1]); 
+				}
+				i++;
+			}
+		}
+		
+		public void determDessert (){
+			o1.description += "\n" + "Desserts ";
+			String[][] dessert = o1.getDessert();
+			int i = 0;
+			for(var b : desCheckBox){
+				if(b.isSelected()){
+					o1.description += dessert[i][0];
+					o1.orderTotal += Double.parseDouble(dessert[i][1]); 
+				}
+				i++;
+			}
+		}
+
+		public void determDrinks(){
+			double numDrinks = Integer.parseInt(txtDrink.getText());
+			o1.orderTotal += numDrinks;
+			o1.description += "and " + (txtDrink.getText()) + " Drinks";
+		}
+		
+		public String getDescription(){
+			return o1.description;
+		}
+	
+		public String getPrice(){
+			o1.orderTotal = (o1.orderTotal * 100) / 100;
+			String price = "The Total is $" + String.valueOf(o1.orderTotal);
+			return price;
+		}
 }
